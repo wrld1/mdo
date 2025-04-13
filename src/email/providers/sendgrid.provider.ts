6e6f92b67b006a71ca77e6675513265012c9721d
Kyrylo Lolenko <kir260302@gmail.com>
2025-04-13 11:53:06 +0300

    Add new table

--- src/email/providers/sendgrid.provider.ts
+++ src/email/providers/sendgrid.provider.ts
@@ -13,31 +13,31 @@ export class SendGridMailService extends AbstractEmailService {
     super();
     const apiKey = process.env.SENDGRID_API_KEY;
 
-    SendGrid.setApiKey(apiKey);
+    // SendGrid.setApiKey(apiKey);
 
     this.defaultFrom = process.env.DEFAULT_SENDER;
   }
 
   async sendEmail(options: EmailOptions): Promise<boolean> {
     try {
-      const msg: MailDataRequired = {
-        to: options.to,
-        from: options.from || this.defaultFrom,
-        subject: options.subject,
-        text: options.text,
-        html: options.html,
-        attachments: options.attachments,
-      };
+      // const msg: MailDataRequired = {
+      //   to: options.to,
+      //   from: options.from || this.defaultFrom,
+      //   subject: options.subject,
+      //   text: options.text,
+      //   html: options.html,
+      //   attachments: options.attachments,
+      // };
 
-      if (options.templateId && options.dynamicTemplateData) {
-        msg['templateId'] = options.templateId;
-        msg['dynamicTemplateData'] = options.dynamicTemplateData;
-      }
+      // if (options.templateId && options.dynamicTemplateData) {
+      //   msg['templateId'] = options.templateId;
+      //   msg['dynamicTemplateData'] = options.dynamicTemplateData;
+      // }
 
-      await SendGrid.send(msg);
-      this.logger.log(
-        `Email sent to ${Array.isArray(options.to) ? options.to.join(', ') : options.to}`,
-      );
+      // await SendGrid.send(msg);
+      // this.logger.log(
+      //   `Email sent to ${Array.isArray(options.to) ? options.to.join(', ') : options.to}`,
+      // );
       return true;
     } catch (error) {
       this.logger.error(`Failed to send email: ${error.message}`, error.stack);
